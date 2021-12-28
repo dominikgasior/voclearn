ee90fb88f84cd3da2e250428ea363dac651b84ef
Dominik <dominik.gasior@valueadd.pl>
2021-12-28 10:54:26 +0100

    feat: main bootstrap file

---
+++ apps/api/src/app/nest-app.ts
@@ -0,0 +1,16 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+
+export const globalRoutePrefix = 'api';
+
+export function appendGlobalSettingsToNestApp(app: INestApplication): void {
+  app.setGlobalPrefix(globalRoutePrefix);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+  app.use(cookieParser());
+}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,19 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import {
+  appendGlobalSettingsToNestApp,
+  globalRoutePrefix,
+} from './app/nest-app';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  appendGlobalSettingsToNestApp(app);
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    Logger.log(
+      'Listening at http://localhost:' + port + '/' + globalRoutePrefix
+    );
   });
 }
 
